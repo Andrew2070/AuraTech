3750f063318ec6f4a07369ed1fe667de2f7301b6
Andrew2070 <flaminsaura11@gmail.com>
2017-03-25 19:53:04 +0500

    Update ICBM Launch Server (Basic).lua

--- DefenseTech/ICBM Launch Server (Basic).lua
+++ DefenseTech/ICBM Launch Server (Basic).lua
@@ -14,7 +14,7 @@ peripheral.find("modem", rednet.open)
 
 --[[Functions]]--
 local accounts = {
-  ["User1"] = "password1", --Replace User1 with your Username, and password1 with your password
+  ["Andrew2070"] = "3765", --Replace User1 with your Username, and password1 with your password
   ["User2"] = "password2"
      
 }
@@ -66,31 +66,24 @@ end
  
 local function printSilos()
   clear()
-    print("==========================")
+    red()
     print("     [Detected  Silos]    ")
     for k, v in ipairs(silos) do
     print("      silo #" .. k .. " id = "..v)
   end
-    print("==========================")
-    term.setBackgroundColor(colors.red)
-    print("                          ")
-    print("                          ")
-    term.setBackgroundColor(colors.blue)
+   red()
 end
  
 --[[Main program]]--
 local valid
-  print("=========================")
-  print("  Skynet Systems Active  ")
-  print("=========================")
- 
-  term.setBackgroundColor(colors.blue)
-  print("=========================")
-  print("  Enter Credentials...   ")
-  print("=========================")
-  write("Username: ")
+  red()
+  print("          Skynet Defense Systems Active  ")
+  red()
+  print("             [Strategic Air Command]   ")
+  red()
+  write("Enter Username: ")
   local input_user = read()
-  write("Password: ")
+  write("Enter Password: ")
   local input_pass = read("*")
  
   for valid_user, valid_pass in pairs(accounts) do
@@ -99,17 +92,18 @@ local valid
  
   if valid then
   term.clear()
-  print("Welcome General " ..input_user)
+  print("            Welcome General " ..input_user)
  
 findSilos()
  while true do
   printSilos()
 red()
-  print("  [Launch Verification]   ")
+
+  print("              [Launch Verification]   ")
 red()
-  print(" Enter Verification Code: ")
+  print("             Enter Verification Code: ")
 red()
-  write("Code:  ")
+  write(" Verification Code:  ")
   input  = read()
   term.setBackgroundColor(colors.blue)
   if input == "exit" then
@@ -118,7 +112,7 @@ red()
      while true do
      sleep(1)
 red()
-      print(" Confirm launch? [Yes/No] ")
+  print("             Confirm launch? [Yes/No] ")
 red()
       write("           >")
         input2 = read()
@@ -130,11 +124,11 @@ term.setCursorPos(1,1)
  local count, x, y, z
   while not (type(count) == "number" and type(x) == "number" and type(y) == "number" and type(z) == "number") do
       red()
-      print("    [Target Selection]    ")
+  print("                [Target Selection]    ")
       red()
       write(" Missile Count: ")
       count = tonumber(read())
-      print("Coordinates:")
+      print("Enter Strike Coordinates:")
       write("X: ")
       x = tonumber(read())
       write("Y: ")
@@ -148,9 +142,10 @@ term.clear()
 term.setCursorPos(1,1)
      
       red()
-      print("Target selection complete")
+  print("               [Strategic Air Command]   ")
+  print("              Target selection complete")
       red()
-      print(" Launching in T-Minus 10  ")
+      print("           Launching in T-Minus 10  ")
       sleep(1)
       print("   9 Seconds to Launch    ")
       sleep(1)
@@ -171,9 +166,9 @@ term.setCursorPos(1,1)
       print("   1 Seconds to Launch    ")
       sleep(1)
       red()
-      print("  Do you wish to Abort [Y/N]?   ")
+  print("  Do you wish to Abort [Y/N]?   ")
       red()
-      write("         >")
+      write(" Do you wish to Abort [Y/N]:  ")
 local abort = read()
 if abort == "Yes" then
 os.reboot()
@@ -187,6 +182,8 @@ end
 end
 end
 end
+
 else print("Access Denied")
 print("Rebooting System")
 os.reboot()
+end
