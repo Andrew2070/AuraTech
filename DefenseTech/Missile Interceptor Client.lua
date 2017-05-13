61e36dcc2873f35683f4246145f821cf7cfb9070
Andrew2070 <flaminsaura11@gmail.com>
2017-05-14 00:09:22 +0500

    Update Missile Interceptor Client.lua
    Bug Fixes

--- DefenseTech/Missile Interceptor Client.lua
+++ DefenseTech/Missile Interceptor Client.lua
@@ -1,72 +1,59 @@
---[[Missile Interceptor Client by: Andrew2070]]--
---[[Warning: 5% chance of interception failure]]--
---[[Don't touch the code unless you know what you're doin]]--
---[[Annotations below describe what each section does]]--
-
-
---[[Initialization]]--
 shell.run("clear")
 shell.run("id")
 missilesilo = peripheral.wrap("back")
 rednet.open("top")
 local armed = true
 local masterID
---local pack = textutils.serialise(siloData)
- 
---[[Contingencies]]--
- 
-if peripheral.call("back", "getMissile") ~= "{}"
-then currentmissile = "No Missile"
-  elseif peripheral.call("back", "getMissile") = "anti-ballistic missile"
-  then
-   local  currentmissile = tostring(missilesilo.getMissile())
-    end
- 
---//Basically provides a contingency that when no missile is detected, it returns "No Missile"
---//Prevents crashing due to nil values returned by empty missile silo.
- 
- 
---[[Data Storage + Table]]--
+local pack = textutils.serialise(siloData)
+local icbm = peripheral.wrap("back")
 
+
+
+if missilesilo.getMissile() ~= "{}" then currentmissile = "None" 
  
+    else
+  
+    local  currentmissile = tostring(missilesilo.getMissile())
+ end
+ 
+     local  armed = tostring(missilesilo.canLaunch())
+ 
+       local targets = tostring(missilesilo.getTarget())
+
+print(currentmissile)
+
 local siloData = {
 ["Msg"] = "ABM",
 ["ID"] = os.getComputerID(),
 ["Missile"] = currentmissile,
-["armed"] = armed
+["Target"] = targets,
+["Armed"] = armed
 }
- 
-siloData.tX, siloData.tY, siloData.tZ = missilesilo.getTarget()
 
 
-
---[[Main Program + Rednet Setup]]--
 while true do
- print("Waiting for Threat Message From Defense Server...")
- local id, msg2 = rednet.receive(masterID)
+  print("Waiting for Threat Message From Defense Server...")
+ local id, msg = rednet.receive(masterID)
  
-  if (msg2 == "ABM1") then
+  if (msg == "ABM1") then
     print("  master=", id)
     masterID = id;
     print(siloData[1])
     rednet.send(masterID, siloData)
- 
---//Top: If a message "ABM1" is received then log the id of the sender and send that ID the data.
---//Bottom: If the message is not "ABM1" and is a "table" from the id of the original sender then prepare for launch.
- 
-            elseif type(msg2) == "table" and id == masterID then
-              if type(msg2.x) == "number" and type(msg2.y) == "number" and type(msg2.z) == "number" then
-              print("  launching CounterMeasures At x=" .. msg2.x .. ", y=" .. msg2.y .. ", z=" .. msg2.z)
-              icbm.setTarget(msg2.x, msg2.y, msg2.z)
- 
+
+
+            elseif type(msg) == "table" and id == masterID then
+              if type(msg.x) == "number" and type(msg.y) == "number" and type(msg.z) == "number" then
+              print("  launching CounterMeasures At x=" .. msg.x .. ", y=" .. msg.y .. ", z=" .. msg.z)
+              icbm.setTarget(msg.x, msg.y, msg.z)
                 if (armed) then
                 peripheral.call("back","launch")
               end
- 
           else
       print("  Invalid Table Command")
     end
   else
  print("  Invalid Message:",msg," from: ",id)
+ 
 end
 end
